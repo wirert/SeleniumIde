cf01c8d86d090664b8bfa9738818f46b1a0eb259
Krasimir Nikolov <krasimir.nikolov@accedia.com>
2024-10-13 10:28:02 +0300

    update test

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -23,12 +23,12 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
-        ChromeOptions options = new ChromeOptions();
-        options.AddArguments("headless");
-        options.AddArguments("no-sandbox");
-        options.AddArguments("disable-dev-shm-usage");
-        options.AddArguments("disable-gpu");
-        options.AddArguments("window-size=1920x1080");
+        //ChromeOptions options = new ChromeOptions();
+        //options.AddArguments("headless");
+        //options.AddArguments("no-sandbox");
+        //options.AddArguments("disable-dev-shm-usage");
+        //options.AddArguments("disable-gpu");
+        //options.AddArguments("window-size=1920x1080");
 
         driver = new ChromeDriver();
         js = (IJavaScriptExecutor)driver;
